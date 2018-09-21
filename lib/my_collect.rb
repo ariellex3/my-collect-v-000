207c076e321ba0d868803fc38340ea2d2d1f3fed
Arielle D <domingu4@gmail.com>
2018-09-21 22:42:32 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,28 +1,2 @@
-def my_collect(array)
-  counter = 0
-
-  while i < array.length
-    i = i + 1
-  end
+def my_collect(empty_array)
 end
-
-
-#collection = ['ruby', 'javascript', 'python', 'objective-c']
-#my_collect(collection) do |lang|
-#  lang.upcase
-#end
-
-
-#take argument of collection
-#iterate over collection using while
-#execute over all using yield
-#return modified collection
-
-
-#def hello_t(array)
-#  i = 0
-
-#  while i < array.length
-#    i = i + 1
-#  end
-#end
